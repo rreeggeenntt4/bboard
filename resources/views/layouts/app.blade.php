7d854be22fe16dcb7bff5242cefd97f08d09944e
rreeggeenntt4 <114906878+rreeggeenntt4@users.noreply.github.com>
2023-03-05 23:17:21 +0300

    up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
 
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="/styles/main.css" type="text/css">
 </head>
 
 <body>
