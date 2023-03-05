9a83aedb5896e522330e3bcf8e8b9c3d65343355
rreeggeenntt4 <114906878+rreeggeenntt4@users.noreply.github.com>
2023-03-05 21:58:26 +0300

    up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,47 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends(loyauts.app)
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('title', $bb->title)
 
-    <title>Объявления</title>
-
-    <!-- Fonts -->
-    <!--     <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" /> -->
-
-    <!-- Styles -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-
-<body class="container">
-    <h1 class="my-3 text-center">Объявления</h1>
-    @if (count($bbs) > 0)
-    <table class="table table-striped table-borderless">
-        <thead>
-            <tr>
-                <th>Товар</th>
-                <th>Цена, руб.</th>
-                <th>&nbsp</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($bbs as $bb)
-            <tr>
-                <td>
-                    <h4>{{$bb->title}}</h4>
-                </td>
-                <td>{{$bb->price}}</td>
-                <td>
-                    <a href="/{{$bb->id}}">Подробнее ...</a>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-    @endif
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-
-</html>
+$section('content')
+<h2>{{$bb->title}}</h2>
+<p>{{$bb->content}}</p>
+<p>{{$bb->price}}</p>
+<p><a href="/">На перечень объявлений</a></p>
+@endsection('content')
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@ield('title')::Объявления</title>
+
+    <!-- Styles -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="my-3 text-center">Объявления</h1>
+        @yeld('content')
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
