ff2d947f8691fa42b86b2d7fcb08e84793371857
rreeggeenntt4 <114906878+rreeggeenntt4@users.noreply.github.com>
2023-03-05 22:15:08 +0300

    up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,6 @@ $section('content')
 <h2>{{$bb->title}}</h2>
 <p>{{$bb->content}}</p>
 <p>{{$bb->price}}</p>
-<p><a href="{{ route('detail',['bb']=>$bb->id) }}">На перечень объявлений</a></p>
+<p><a href="{{ route('detail',['bb'=>$bb->id]) }}">На перечень объявлений</a></p>
 @endforeach
 @endsection('content')
