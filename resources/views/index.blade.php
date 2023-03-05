d001f4f3829aae08afe6741619df1e7356e1c6c3
rreeggeenntt4 <114906878+rreeggeenntt4@users.noreply.github.com>
2023-03-05 22:42:04 +0300

    up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,10 +3,30 @@
 @section('title', 'Главная')
 
 @section('content')
-@foreach($bbs as $bb)
-<h2>{{$bb->title}}</h2>
-<p>{{$bb->content}}</p>
-<p>{{$bb->price}}</p>
-<p><a href="{{ route('detail',['bb'=>$bb->id]) }}">Подробнее ...</a></p>
-@endforeach
+
+@if (count($bbs) > 0)
+<table class="table table-striped table-borderless">
+    <thead>
+        <tr>
+            <th>Товар</th>
+            <th>Цена, руб.</th>
+            <th>&nbsp</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($bbs as $bb)
+        <tr>
+            <td>
+                <h4>{{$bb->title}}</h4>
+            </td>
+            <td>{{$bb->price}}</td>
+            <td>
+                <a href="{{ route('detail',['bb'=>$bb->id]) }}">Подробнее ...</a>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+@endif
+
 @endsection('content')
