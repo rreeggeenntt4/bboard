4a68d42dec254875be5a8c663a94b249085d960b
rreeggeenntt4 <114906878+rreeggeenntt4@users.noreply.github.com>
2023-03-05 22:45:05 +0300

    up

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,25 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('title', $bb->title)
 
-    <title>{{$bb->title}} ::Объявления</title>
-
-    <!-- Styles -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-
-<body class="container">
-    <h1 class="my-3 text-center">Объявления</h1>
-
-    <h2>{{$bb->title}}</h2>
-    <p>{{$bb->content}}</p>
-    <p>{{$bb->price}}</p>
-    <p><a href="{{ route('index') }}">На перечень объявлений</a></p>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-
-</html>
+@section('content')
+<h2>{{$bb->title}}</h2>
+<p>{{$bb->content}}</p>
+<p>{{$bb->price}}</p>
+<p><a href="{{ route('index') }}">На перечень объявлений</a></p>
+@endsection('content')
